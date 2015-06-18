4d658f0613f7d3353592995d1c49314a9ffc0857
scheelmichael <scheelmichael@gmail.com>
2015-06-18 07:09:28 +0200

    Assignment 2 finished
    Taking cacheMean.R as an example

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+## it will create a list containing the original matrix and the inverse of if
 
-## I have made my first edits to this file
-## now I have made my second edits to this file
+## main goal is to avoid costly resolving for a matrix
+## sample invertible matrix a <- matrix(c(1,-1,2,10,-1,3,-3,-4,3), nrow = 3, ncol = 3,)
 
+## First Function to return a list with data and the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        ## set function
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        ## get function 
+        get <- function() x
+        ## set inverse
+        set_inverse <- function(solve) m <<- solve
+        ## get inverse
+        get_inverse <- function() m
+        ## return list
+        list(set = set, get = get,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
+## The second function is going to utilize the functions from above to perform the 
+## cache checking and calculation in the event that the cache is not present. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$get_inverse()
+        if(!is.null(m)) {
+                message("getting cached inverse")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$set_inverse(m)
+        m
 }
+
+
+
+
+
