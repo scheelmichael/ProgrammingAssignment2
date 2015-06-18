9fc01fee68d85afe37f32ebfe484096ab8cc9165
scheelmichael <scheelmichael@gmail.com>
2015-06-18 07:11:49 +0200

    Finished Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
 ## This function creates a special "matrix" object that can cache its inverse
 ## it will create a list containing the original matrix and the inverse of if
